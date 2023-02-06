d7222179dbd91087d6edd8dd1f7b8a2f94943c8c
matheuscmartins <matheus87martins@outlook.com>
2023-02-06 16:15:31 -0300

    endpoint findById Tecnico

--- src/main/java/br/com/homeoffice/helpdesk/resources/TecnicoResource.java
+++ src/main/java/br/com/homeoffice/helpdesk/resources/TecnicoResource.java
@@ -1,2 +1,24 @@
-package br.com.homeoffice.helpdesk.resources;public class TecnicoResource {
+package br.com.homeoffice.helpdesk.resources;
+
+import br.com.homeoffice.helpdesk.domain.Tecnico;
+import br.com.homeoffice.helpdesk.services.TecnicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/tecnicos")
+public class TecnicoResource {
+
+    @Autowired
+    private TecnicoService tecnicoService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+    Tecnico obj = tecnicoService.findById(id);
+    return ResponseEntity.ok().body(obj);
+    }
 }
--- src/main/java/br/com/homeoffice/helpdesk/services/TecnicoService.java
+++ src/main/java/br/com/homeoffice/helpdesk/services/TecnicoService.java
@@ -1,2 +1,19 @@
-package br.com.homeoffice.helpdesk.services;public class TecnicoService {
+package br.com.homeoffice.helpdesk.services;
+
+import br.com.homeoffice.helpdesk.domain.Tecnico;
+import br.com.homeoffice.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+
+    public Tecnico findById(Integer id){
+        Optional<Tecnico> obj = tecnicoRepository.findById(id);
+        return obj.orElse(null);
+    }
 }
