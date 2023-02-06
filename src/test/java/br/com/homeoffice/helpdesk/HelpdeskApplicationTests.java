9ac155d2e554b5782ced42eed021e91b379ec124
matheuscmartins <matheus87martins@outlook.com>
2023-02-06 08:50:50 -0300

    created project

---
+++ src/test/java/br/com/homeoffice/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.homeoffice.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
