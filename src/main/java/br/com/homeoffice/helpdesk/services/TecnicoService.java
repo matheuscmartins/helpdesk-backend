321edfd75fdc01463f77eb44e0ced970329a2b6d
matheuscmartins <matheus87martins@outlook.com>
2023-02-07 14:16:10 -0300

    Endpoint findAll tecnico

--- src/main/java/br/com/homeoffice/helpdesk/services/TecnicoService.java
+++ src/main/java/br/com/homeoffice/helpdesk/services/TecnicoService.java
@@ -6,6 +6,7 @@ import br.com.homeoffice.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,5 +17,9 @@ public class TecnicoService {
     public Tecnico findById(Integer id){
         Optional<Tecnico> obj = tecnicoRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
+    }
+
+    public List<Tecnico> findAll() {
+        return tecnicoRepository.findAll();
     }
 }
