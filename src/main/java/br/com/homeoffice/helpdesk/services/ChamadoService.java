eb925e489650790faf454610328f74a2eac7ade6
matheuscmartins <matheus87martins@outlook.com>
2023-02-08 10:12:43 -0300

    "findAll for chamado"

--- src/main/java/br/com/homeoffice/helpdesk/services/ChamadoService.java
+++ src/main/java/br/com/homeoffice/helpdesk/services/ChamadoService.java
@@ -6,6 +6,7 @@ import br.com.homeoffice.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,4 +19,7 @@ public class ChamadoService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não econtrado! ID: " + id));
     }
 
+    public List<Chamado> findAll() {
+        return chamadoRepository.findAll();
+    }
 }
